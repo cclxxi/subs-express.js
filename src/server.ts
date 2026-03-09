84ba57b0964c00945f6b442bf5356effbad2b29b
cclxxi <ilya.proshin82@gmail.com>
2026-03-09 11:58:28 +0600

    feat: update start script to use ts-node and load environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from "./app"
 import * as process from "node:process";
 
+process.loadEnvFile()
+
 async function start() {
     app.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
