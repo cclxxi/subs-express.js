4d67745f89dd8b99e5d03eb4879713a23849c2cf
cclxxi <ilya.proshin82@gmail.com>
2026-03-10 18:35:52 +0600

    feat(schema): rename last field to last4 in CreateCardSchema

--- src/validators/createCard.schema.ts
+++ src/validators/createCard.schema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const CreateCardSchema = z.object({
     name: z.string().min(2).max(255),
-    last: z.number().min(1000).max(9999),
+    last4: z.number().min(1000).max(9999),
 });
