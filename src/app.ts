0f85afddc6dbc2cfef6eb088f21fe74044bfc8bd
cclxxi <ilya.proshin82@gmail.com>
2026-03-10 20:52:20 +0600

    feat(cors): add CORS support with specific origin and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,18 @@ import userRoutes from './routes/user.routes';
 import authRoutes from "./routes/auth.routes";
 import subscriptionRoutes from "./routes/subscription.routes";
 import cardRoutes from "./routes/card.routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: ["https://subs-reactjs.vercel.app"]
+}))
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/docs.json", (_req, res) => {
+    res.json(swaggerSpec);
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
